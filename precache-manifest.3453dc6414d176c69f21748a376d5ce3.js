549eee126a8dc892dfc4a801dd2603181cc6a828
BedoyaAlban <a.bedoya@outlook.fr>
2020-09-01 16:31:19 +0200

    Updates

---
+++ precache-manifest.3453dc6414d176c69f21748a376d5ce3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b1f04ced2d1bd74400ceb5c1f62328",
+    "url": "/reader-app/index.html"
+  },
+  {
+    "revision": "0dab98440312e9bb0f0c",
+    "url": "/reader-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c589fc5603146bac3a7e",
+    "url": "/reader-app/static/js/2.b729121a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reader-app/static/js/2.b729121a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dab98440312e9bb0f0c",
+    "url": "/reader-app/static/js/main.def6191a.chunk.js"
+  },
+  {
+    "revision": "8b332c84a2af73474aea",
+    "url": "/reader-app/static/js/runtime-main.378f4ace.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reader-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
